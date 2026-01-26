8ac7b345f21fa13c5f974f2c5aa53bcfcecae044
chabin0323-dev <chabin0323@gmail.com>
2026-01-26 22:38:13 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-black text-white antialiased">{children}</body>
     </html>
   );
 }
